46e144fe1d954006c3d34d23d94ab5de73d52cfd
Ayush Nainwal <aayu@Ayushs-MacBook-Air.local>
2024-05-02 11:00:17 +0530

    Second commit

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center jusifty-center px-4 md:px-8 xl:px-10 py-44">
-      {/* <div className="grid xl:grid-cols-3 lg:grid-cols-2 w-full gap-10 max-w-[1400px]">
+      <div className="grid xl:grid-cols-3 lg:grid-cols-2 w-full gap-10 max-w-[1400px]">
         <GridItem title="Area Chart">
           <AreaChartComponent/>
         </GridItem>
@@ -18,8 +18,8 @@ export default function Home() {
           <LineChartComponent/>
         </GridItem>
 
-      </div> */}
-      <AreaChartComplete/>
+      </div>
+     
     </main>
   );
 }
